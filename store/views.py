e8026744ba2c8dedb5e0347addbb835877492484
hyunwoo <ashyonu@gmail.com>
2023-01-05 23:32:35 +0900

    feat: add store signin

--- store/views.py
+++ store/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from store.models import Store
 
-# Create your views here.
+
+@api_view(['POST'])
+def signin(request):
+    store_name = request.data['store_name']
+    phone_num = request.data['phone_num']
+    address = request.data['address']
+
+    try:
+        Store.objects.create(store_name=store_name, phone_num=phone_num, address=address)
+    except:
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+    return Response(status=status.HTTP_201_CREATED)
