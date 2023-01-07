3e8dcbd849fa04cc90e02e5c54abcf6ecafde0d2
ggamD00 <jaehuek@naver.com>
2023-01-07 16:19:27 +0900

    feat: waiting already exists

--- waiting/views.py
+++ waiting/views.py
@@ -1,6 +1,6 @@
 import json
 
-from django.http import JsonResponse
+from django.http import HttpResponse
 from rest_framework.decorators import api_view
 
 from store.models import Store
@@ -10,19 +10,27 @@ from waiting.models import Waiting
 @api_view(['GET', 'POST', 'PATCH'])
 def waiting(request):
     if request.method == 'POST':
-        try:
-            data = json.loads(request.body)
+        data = json.loads(request.body)
 
-            store_id = Store.objects.get(store_id=data["store_id"])
-            name = data["name"]
-            phone_num = data["phone_num"]
-            people = data["people"]
-            password = data["password"]
-            status = data["status"]
+        phone_num = data["phone_num"]
+        try:
+            waiting_check = Waiting.objects.filter(phone_num=phone_num, status="WA")
+            if len(waiting_check) != 0:
+                return HttpResponse({"이미 진행 중인 웨이팅이 있습니다!"}, status=400)
+        except Waiting.DoesNotExist:
+            pass
+
+        store_id = Store.objects.get(store_id=data["store_id"])
+        name = data["name"]
+        people = data["people"]
+        password = data["password"]
+        status = data["status"]
+
+        try:
             Waiting.objects.create(store_id=store_id, name=name, phone_num=phone_num,
                                    people=people, password=password, status=status)
 
-            return JsonResponse({""}, status=201)
+            return HttpResponse(status=201)
         except Exception:
-            return JsonResponse({""}, status=400)
+            return HttpResponse(status=400)
 
