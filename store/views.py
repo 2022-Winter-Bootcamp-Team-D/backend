07058d78639e6d41bec33755890541e7f73dc2ba
hyunwoo <ashyonu@gmail.com>
2023-01-07 15:26:07 +0900

    feat:  add store_id in response body

--- store/views.py
+++ store/views.py
@@ -2,17 +2,22 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from store.models import Store
+from .serializer import StoreJoinSerializer
 
 
 @api_view(['POST'])
 def signin(request):
     store_name = request.data['store_name']
     phone_num = request.data['phone_num']
-    address = request.data['address']
+    latitude = request.data['latitude']
+    longitude = request.data['longitude']
     password = request.data['password']
 
     try:
-        Store.objects.create(store_name=store_name, phone_num=phone_num, address=address, password=password)
+        object = Store.objects.create(store_name=store_name, phone_num=phone_num, latitude=latitude, longitude=longitude,
+                             password=password)
+        response = StoreJoinSerializer(object)
     except:
         return Response(status=status.HTTP_400_BAD_REQUEST)
-    return Response(status=status.HTTP_201_CREATED)
+    return Response(data=response.data, status=status.HTTP_201_CREATED)
+
