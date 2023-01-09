6cea0b1e4ebe03d73e85f7db65e7063d1f967550
chynmn <chynmn0523@gmail.com>
2023-01-09 22:59:07 +0900

    [feat] PATCH API to change breaktime

--- store/views.py
+++ store/views.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from store.models import Store
 from .serializer import StoreJoinSerializer
+from .serializer import StoreBreaktimeSerializer
 
 
 @api_view(['POST'])
@@ -12,12 +13,26 @@ def signin(request):
     latitude = request.data['latitude']
     longitude = request.data['longitude']
     password = request.data['password']
+    information = request.data['information']
 
     try:
         object = Store.objects.create(store_name=store_name, phone_num=phone_num, latitude=latitude, longitude=longitude,
-                             password=password)
+                                      password=password, information=information)
         response = StoreJoinSerializer(object)
     except:
         return Response(status=status.HTTP_400_BAD_REQUEST)
     return Response(data=response.data, status=status.HTTP_201_CREATED)
 
+
+@api_view(['PATCH'])
+def breaktime(request):
+    store_id = request.data['store_id']
+
+    try:
+        object = Store.objects.get(store_id=store_id)
+        object.is_waiting = not object.is_waiting
+        object = object.save()
+
+    except:
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+    return Response(status=status.HTTP_200_OK)
