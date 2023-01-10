f899e309f84ce4623436dee1d901dd2ba5226b11
chynmn <chynmn0523@gmail.com>
2023-01-11 00:08:00 +0900

    [feat] Add GET API to refer waiting list

--- store/views.py
+++ store/views.py
@@ -2,9 +2,11 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from store.models import Store
+from waiting.models import Waiting
 from .serializer import StoreJoinSerializer
 from .serializer import StoreBreaktimeSerializer
 from .serializer import StoreDetailSerializer
+from .serializer import StoreWaitingsSerializer
 
 
 @api_view(['POST'])
@@ -48,6 +50,20 @@ def detail(request):
         object.information = information
         object = object.save()
 
+    except:
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+    return Response(status=status.HTTP_200_OK)
+
+
+@api_view(['GET'])
+def waitings(request):
+    store_id = request.data['store_id']
+
+    try:
+        object = Store.objects.get(store_id=store_id)
+        object = Waiting.objects.filter(store_id=object, status='WA')
+
+        response = StoreWaitingsSerializer(object)
     except:
         return Response(status=status.HTTP_400_BAD_REQUEST)
     return Response(status=status.HTTP_200_OK)
