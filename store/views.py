a3528d53edcd4f3fd7547483cf53c45e5b95e26c
chynmn <chynmn0523@gmail.com>
2023-01-12 22:07:20 +0900

    [fix] clean code

--- store/views.py
+++ store/views.py
@@ -74,32 +74,26 @@ class waitings(APIView):
     def get(self, request):
         store_id = request.data['store_id']
         WA = 'WA'
-        # try:
-        store = Store.objects.get(store_id=store_id)
-        waitings = Waiting.objects.raw(
-            """SELECT waiting_id, name, people, phone_num FROM Waiting WHERE store_id=%s AND status=%s""" % (store_id, "'WA'"))
-        waiting = serializers.serialize(
-            "json", waitings, fields=("phone_num", "people", "name"))
-        data = {}
-        data["data"] = []
-        for i in waitings:
-            temp = {
-                "waiting_id": i.pk,
-                "name": i.name,
-                "phone_num": i.phone_num,
-                "people": i.people
-            }
-            data["data"].append(temp)
-        data["information"] = store.information
-        data["is_waiting"] = store.is_waiting
-        # print(waiting[0])
-        # print(waitings)
-        # if waitings == null:
-        # waitings.information = store.information
-        # waitings.is_waiting = store.is_waiting
-        # response = StoreWaitingsSerializer(waitings)
-        # except:
-        #     return Response(status=status.HTTP_400_BAD_REQUEST)
+        try:
+            store = Store.objects.get(store_id=store_id)
+            waitings = Waiting.objects.raw(
+                """SELECT waiting_id, name, people, phone_num FROM Waiting WHERE store_id=%s AND status=%s""" % (store_id, "'WA'"))
+            waiting = serializers.serialize(
+                "json", waitings, fields=("phone_num", "people", "name"))
+            data = {}
+            data["data"] = []
+            for i in waitings:
+                temp = {
+                    "waiting_id": i.pk,
+                    "name": i.name,
+                    "phone_num": i.phone_num,
+                    "people": i.people
+                }
+                data["data"].append(temp)
+            data["information"] = store.information
+            data["is_waiting"] = store.is_waiting
+        except:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
         return Response(data, status=status.HTTP_200_OK, content_type="text/json-comment-filtered")
 
     def patch(self, request):
