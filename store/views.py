a5f70dd2b8b7c3b7f3654a0c9ec4c08b6d37dcde
ggamD00 <jaehuek@naver.com>
2023-01-13 14:51:35 +0900

    chore: refactoring of store/view

--- store/views.py
+++ store/views.py
@@ -1,18 +1,12 @@
-from django.http import HttpResponse
 from rest_framework import status
 from rest_framework.response import Response
-from django.http import HttpResponse
 from rest_framework.decorators import api_view
 from rest_framework.views import APIView
 from store.models import Store
-from user.models import User
 from waiting.models import Waiting
 from user.models import User
-from django.core import serializers
 from .serializer import StoreJoinSerializer
 from .notification import notify
-from .serializer import StoreWaitingsSerializer
-import json
 
 
 @api_view(['POST'])
@@ -70,49 +64,47 @@ def detail(request):
     return Response(status=status.HTTP_200_OK)
 
 
+# 가게의 웨이팅 목록, 상세정보, 웨이팅 받는지 여부를 반환
+def search_waitings(store_id):
+    store = Store.objects.get(store_id=store_id)
+    data = {}
+    data["data"] = []
+    data["information"] = store.information
+    data["is_waiting"] = store.is_waiting
+
+    # 가게 웨이팅이 없을경우 상세정보랑 웨이팅 상태만 반환
+    try:
+        waitings = Waiting.objects.raw(
+            """SELECT waiting_id
+            FROM Waiting 
+            WHERE store_id=%s AND status=%s""" % (store_id, "'WA'"))
+    except:
+        return Response(data, status=status.HTTP_200_OK, content_type="text/json-comment-filtered")
+
+    for i in waitings:
+        temp = {
+            "waiting_id": i.pk,
+            "name": i.name,
+            "phone_num": i.phone_num,
+            "people": i.people
+        }
+        data["data"].append(temp)
+    return data
+
+
 class waitings(APIView):
     def get(self, request):
         store_id = request.data['store_id']
-        WA = 'WA'
-        # try:
-        store = Store.objects.get(store_id=store_id)
-        waitings = Waiting.objects.raw(
-            """SELECT waiting_id, name, people, phone_num FROM Waiting WHERE store_id=%s AND status=%s""" % (store_id, "'WA'"))
-        waiting = serializers.serialize(
-            "json", waitings, fields=("phone_num", "people", "name"))
-        data = {}
-        data["data"] = []
-        for i in waitings:
-            temp = {
-                "waiting_id": i.pk,
-                "name": i.name,
-                "phone_num": i.phone_num,
-                "people": i.people
-            }
-            data["data"].append(temp)
-        data["information"] = store.information
-        data["is_waiting"] = store.is_waiting
-        # print(waiting[0])
-        # print(waitings)
-        # if waitings == null:
-        # waitings.information = store.information
-        # waitings.is_waiting = store.is_waiting
-        # response = StoreWaitingsSerializer(waitings)
-        # except:
-        #     return Response(status=status.HTTP_400_BAD_REQUEST)
+        data = search_waitings(store_id)
         return Response(data, status=status.HTTP_200_OK, content_type="text/json-comment-filtered")
 
     def patch(self, request):
-        store_id = request.data['store_id']
         waiting_id = request.data['waiting_id']
 
         try:
             waiting = Waiting.objects.get(waiting_id=waiting_id)
             waiting.status = 'EN'
             waiting.save()
-
-            # response = StoreWaitingsSerializer(object)
-            # enter_notify(request)
         except:
             return Response(status=status.HTTP_400_BAD_REQUEST)
         return Response(status=status.HTTP_200_OK)
@@ -123,45 +115,26 @@ def search_waiting_order(waiting_id, store_id):
     waiting_order = len(waiting_teams) + 1
     return waiting_order
 
+
 @api_view(['PATCH'])
 def cancellations(request):
     waiting_id = request.data['waiting_id']
     store_id = request.data['store_id']
     waiting_order = search_waiting_order(waiting_id, store_id)
-    data = {}
     try:
         cancel_token = User.objects.get(waiting_id=waiting_id).token
-        store = Store.objects.get(store_id=store_id)
 
         # status를 CN(CANCEL)로 바꿔주고 취소 알림 보내기
         Waiting.objects.filter(waiting_id=waiting_id, store_id=store_id).update(status='CN')
         notify.cancel_notify(cancel_token)
 
         # 다음 웨이팅 팀이 존재하는지 확인 없으면 바로 리턴
-        try:
-            waitings = Waiting.objects.raw(
-                """SELECT waiting_id
-                FROM Waiting 
-                WHERE store_id=%s AND status=%s""" % (store_id, "'WA'"))
-            auto_token = User.objects.get(waiting_id=waitings[0]).token
-        except :
-            return Response(data, status=status.HTTP_200_OK, content_type="text/json-comment-filtered")
+        data = search_waitings(store_id)
+        auto_token = User.objects.get(waiting_id=data["data"][0]['waiting_id']).token
 
         # 취소한 웨이팅이 1순위였고 다음 웨이팅 팀이 존재할 경우 다음 팀에게 1순위 알림 보내기
         if waiting_order == 1:
             notify.auto_notify(auto_token)
-
-        data["data"] = []
-        for i in waitings:
-            temp = {
-                "waiting_id": i.pk,
-                "name": i.name,
-                "phone_num": i.phone_num,
-                "people": i.people
-            }
-            data["data"].append(temp)
-        data["information"] = store.information
-        data["is_waiting"] = store.is_waiting
     except:
         return Response(status=status.HTTP_400_BAD_REQUEST)
 
