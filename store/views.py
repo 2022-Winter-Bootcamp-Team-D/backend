2ca9e9ab301d68d61034205e1a6ca29e8f64b8cb
ggamD00 <jaehuek@naver.com>
2023-01-15 17:05:30 +0900

    feat: add swagger in store and change to CBV

--- store/views.py
+++ store/views.py
@@ -1,67 +1,79 @@
+from django.db import transaction
+from drf_yasg.utils import swagger_auto_schema
 from rest_framework import status
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
 from rest_framework.views import APIView
 from store.models import Store
+from swagger.serializer import SwaggerStoreSigninSerializer, SwaggerStoreWaitingsPostSerializer, \
+    SwaggerStoreWaitingsPatchSerializer, SwaggerStoreEnterNotifySerializer, SwaggerStoreBreakTimeSerializer, \
+    SwaggerStoreDetailSerializer
 from waiting.models import Waiting
 from user.models import User
 from .serializer import StoreJoinSerializer
 from .notification import notify
 
 
-@api_view(['POST'])
-def signin(request):
-    store_name = request.data['store_name']
-    phone_num = request.data['phone_num']
-    latitude = request.data['latitude']
-    longitude = request.data['longitude']
-    password = request.data['password']
+class Signin(APIView):
+    @swagger_auto_schema(tags=['Store'], request_body=SwaggerStoreSigninSerializer)
+    @transaction.atomic
+    def post(self, request):
+        store_name = request.data['store_name']
+        phone_num = request.data['phone_num']
+        latitude = request.data['latitude']
+        longitude = request.data['longitude']
+        password = request.data['password']
 
-    try:
-        object = Store.objects.create(store_name=store_name, phone_num=phone_num, latitude=latitude,
-                                      longitude=longitude,
-                                      password=password)
-        response = StoreJoinSerializer(object)
-    except:
-        return Response(status=status.HTTP_400_BAD_REQUEST)
-    return Response(data=response.data, status=status.HTTP_201_CREATED)
-
-
-@api_view(['POST'])
-def enter_notify(request):
-    # result = notify.enter_notify(request)
-    notify.enter_notify(token=request.data['token'])
-
-    return Response('호출에 성공했습니다!', status=status.HTTP_200_OK)
+        try:
+            object = Store.objects.create(store_name=store_name, phone_num=phone_num, latitude=latitude,
+                                          longitude=longitude,
+                                          password=password)
+            response = StoreJoinSerializer(object)
+        except:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+        return Response(data=response.data, status=status.HTTP_201_CREATED)
 
 
-@api_view(['PATCH'])
-def breaktime(request):
-    store_id = request.data['store_id']
+class Enter_notify(APIView):
+    @swagger_auto_schema(tags=['Store'], request_body=SwaggerStoreEnterNotifySerializer)
+    @transaction.atomic
+    def post(self, request):
+        # result = notify.enter_notify(request)
+        notify.enter_notify(token=request.data['token'])
 
-    try:
-        object = Store.objects.get(store_id=store_id)
-        object.is_waiting = not object.is_waiting
-        object = object.save()
-
-    except:
-        return Response(status=status.HTTP_400_BAD_REQUEST)
-    return Response(status=status.HTTP_200_OK)
+        return Response('호출에 성공했습니다!', status=status.HTTP_200_OK)
 
 
-@api_view(['PATCH'])
-def detail(request):
-    store_id = request.data['store_id']
-    information = request.data['information']
+class Breaktime(APIView):
+    @swagger_auto_schema(tags=['Store'], request_body=SwaggerStoreBreakTimeSerializer)
+    @transaction.atomic
+    def patch(self, request):
+        store_id = request.data['store_id']
 
-    try:
-        object = Store.objects.get(store_id=store_id)
-        object.information = information
-        object = object.save()
+        try:
+            object = Store.objects.get(store_id=store_id)
+            object.is_waiting = not object.is_waiting
+            object = object.save()
 
-    except:
-        return Response(status=status.HTTP_400_BAD_REQUEST)
-    return Response(status=status.HTTP_200_OK)
+        except:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+        return Response(status=status.HTTP_200_OK)
+
+
+class Detail(APIView):
+    @swagger_auto_schema(tags=['Store'], request_body=SwaggerStoreDetailSerializer)
+    @transaction.atomic
+    def patch(self, request):
+        store_id = request.data['store_id']
+        information = request.data['information']
+
+        try:
+            object = Store.objects.get(store_id=store_id)
+            object.information = information
+            object = object.save()
+
+        except:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+        return Response(status=status.HTTP_200_OK)
 
 
 # 가게의 웨이팅 목록, 상세정보, 웨이팅 받는지 여부를 반환
@@ -73,13 +85,10 @@ def search_waitings(store_id):
     data["is_waiting"] = store.is_waiting
 
     # 가게 웨이팅이 없을경우 상세정보랑 웨이팅 상태만 반환
-    try:
-        waitings = Waiting.objects.raw(
-            """SELECT waiting_id
-            FROM Waiting 
-            WHERE store_id=%s AND status=%s""" % (store_id, "'WA'"))
-    except:
-        return Response(data, status=status.HTTP_200_OK, content_type="text/json-comment-filtered")
+    waitings = Waiting.objects.raw(
+        """SELECT waiting_id
+        FROM Waiting 
+        WHERE store_id=%s AND status=%s ORDER BY waiting_id""" % (store_id, "'WA'"))
 
     for i in waitings:
         temp = {
@@ -98,13 +107,17 @@ def search_waiting_order(waiting_id, store_id):
     return waiting_order
 
 
-class waitings(APIView):
+class Waitings(APIView):
 
-    def get(self, request):
+    @swagger_auto_schema(tags=['Store'], request_body=SwaggerStoreWaitingsPostSerializer)
+    @transaction.atomic
+    def post(self, request):
         store_id = request.data['store_id']
         data = search_waitings(store_id)
         return Response(data, status=status.HTTP_200_OK, content_type="text/json-comment-filtered")
 
+    @swagger_auto_schema(tags=['Store'], request_body=SwaggerStoreWaitingsPatchSerializer)
+    @transaction.atomic
     def patch(self, request):
         store_id = request.data['store_id']
         waiting_id = request.data['waiting_id']
@@ -123,26 +136,30 @@ class waitings(APIView):
         return Response('대기 1순위 손님에게 알림을 보냈습니다!', status=status.HTTP_200_OK)
 
 
-@api_view(['PATCH'])
-def cancellations(request):
-    waiting_id = request.data['waiting_id']
-    store_id = request.data['store_id']
-    waiting_order = search_waiting_order(waiting_id, store_id)
-    try:
+class Cancellations(APIView):
+    @swagger_auto_schema(tags=['Store'], request_body=SwaggerStoreWaitingsPatchSerializer)
+    @transaction.atomic
+    def patch(self, request):
+        waiting_id = request.data['waiting_id']
+        store_id = request.data['store_id']
+        waiting_order = search_waiting_order(waiting_id, store_id)
+
         cancel_token = User.objects.get(waiting_id=waiting_id).token
 
         # status를 CN(CANCEL)로 바꿔주고 취소 알림 보내기
         Waiting.objects.filter(waiting_id=waiting_id, store_id=store_id).update(status='CN')
         notify.cancel_notify(cancel_token)
 
-        # 다음 웨이팅 팀이 존재하는지 확인 없으면 바로 리턴
+        # 가게의 웨이팅 리스트, 상세 정보, 웨이팅 받는지 여부를 받아 온다.
         data = search_waitings(store_id)
-        auto_token = User.objects.get(waiting_id=data["data"][0]['waiting_id']).token
+        try:
+            auto_token = User.objects.get(waiting_id=data["data"][0]['waiting_id']).token
 
-        # 취소한 웨이팅이 1순위였고 다음 웨이팅 팀이 존재할 경우 다음 팀에게 1순위 알림 보내기
-        if waiting_order == 1:
-            notify.auto_notify(auto_token)
-    except:
-        return Response(status=status.HTTP_400_BAD_REQUEST)
+            # 취소한 웨이팅이 1순위였고 다음 웨이팅 팀이 존재할 경우 다음 팀에게 1순위 알림 보내기
+            if waiting_order == 1:
+                notify.auto_notify(auto_token)
+        # 가게의 웨이팅이 없는 경우
+        except IndexError:
+            pass
 
-    return Response(data, status=status.HTTP_200_OK, content_type="text/json-comment-filtered")
+        return Response(data, status=status.HTTP_200_OK, content_type="text/json-comment-filtered")
