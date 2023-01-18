70f0c152ef1efe8ec12376787574214664d73a81
ggamD00 <jaehuek@naver.com>
2023-01-18 14:57:42 +0900

    feat: add user signup and signin

--- users/views.py
+++ users/views.py
@@ -1,3 +1,59 @@
-from django.shortcuts import render
+# 회원가입
+from django.contrib.auth import authenticate
+from rest_framework import status
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
-# Create your views here.
+from users.serializer import SignupSerializer, SigninSerializer
+
+
+class SignupAPIView(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        serializer = SignupSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            # jwt token 접근해주기
+            token = TokenObtainPairSerializer.get_token(user)
+            refresh_token = str(token)
+            access_token = str(token.access_token)
+            res = Response(
+                {
+                    "access": access_token,
+                    "refresh": refresh_token,
+                }
+            )
+            res.set_cookie("refresh", refresh_token, httponly=True)
+            return Response(res.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+# 로그인
+class SigninView(APIView):
+
+    def post(self, request):
+        user = authenticate(
+            email=request.data.get("email"), password=request.data.get("password")
+        )
+        if user is not None:
+            serializer = SigninSerializer(user)
+            token = TokenObtainPairSerializer.get_token(user)
+            refresh_token = str(token)
+            access_token = str(token.access_token)
+            res = Response(
+                {
+                    "user": serializer.data,
+                    "message": "login success",
+                    "token": {
+                        "access": access_token,
+                        "refresh": refresh_token,
+                    },
+                },
+                status=status.HTTP_200_OK,
+            )
+            return res
+        else:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
