6a9cad1182d50b76874277640aa70e388b30783d
ggamD00 <jaehuek@naver.com>
2023-01-18 14:55:21 +0900

    feat: add User model

--- users/models.py
+++ users/models.py
@@ -2,7 +2,6 @@ from waiting.models import Waiting
 from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
 from django.contrib.auth.models import PermissionsMixin
 from django.db import models
-from datetime import datetime
 
 
 class Token(models.Model):
@@ -20,7 +19,7 @@ class UserManager(BaseUserManager):
     for authentication instead of usernames.
     """
 
-    def create_user(self, email, password):
+    def create_user(self, email, password, name, phone_num):
         """
         Create and save a User with the given email and password.
         """
@@ -32,27 +31,31 @@ class UserManager(BaseUserManager):
         )
         user.set_password(password)
         # users.set_is_staff(False)
-        user.save()
+        user.save(using=self.db)
         return user
 
-    def create_superuser(self, email, password):
+    def create_superuser(self, email, password, name, phone_num):
         """
         Create and save a SuperUser with the given email and password.
         """
-        user = self.create_user(
+        superuser = self.create_user(
+            name=name,
+            phone_num=phone_num,
             email=email,
             password=password
         )
-        user.is_staff = True
-        user.save()
-        return user
+        superuser.is_staff = True
+        superuser.save(using=self.db)
+        return superuser
 
 
 class User(AbstractBaseUser):
+    name = models.CharField(max_length=50)
+    phone_num = models.CharField(max_length=13)
     email = models.EmailField(unique=True, max_length=255)
     is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
-    created_at = models.DateTimeField(default=datetime.now)
+    created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
