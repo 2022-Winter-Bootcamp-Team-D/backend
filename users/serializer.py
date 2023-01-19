3f77e709de4ba21b879ca2f4ba40dbb10dbae862
ggamD00 <jaehuek@naver.com>
2023-01-20 03:38:43 +0900

    chore: modify serializer

--- users/serializer.py
+++ users/serializer.py
@@ -14,18 +14,35 @@ class SignupSerializer(serializers.ModelSerializer):
         phone_num = validated_data.get('phone_num')
         email = validated_data.get('email')
         password = validated_data.get('password')
+        role = 'auth'
         user = User(
             name=name,
             phone_num=phone_num,
-            email=email
+            email=email,
+            role=role
         )
         user.set_password(password)
         user.save()
         return user
 
 
-class SigninSerializer(serializers.ModelSerializer):
+class StoreSignupSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = '__all__'
 
+    def create(self, validated_data):
+        name = validated_data.get('name')
+        phone_num = validated_data.get('phone_num')
+        email = validated_data.get('email')
+        password = validated_data.get('password')
+        role = 'store'
+        user = User(
+            name=name,
+            phone_num=phone_num,
+            email=email,
+            role=role
+        )
+        user.set_password(password)
+        user.save()
+        return user
