39a7af3e961b17e04cc2434e6e27b8f4ff803f46
ggamD00 <jaehuek@naver.com>
2023-01-20 03:35:34 +0900

    chore: modify models

--- users/models.py
+++ users/models.py
@@ -1,16 +1,6 @@
-from waiting.models import Waiting
 from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
 from django.contrib.auth.models import PermissionsMixin
 from django.db import models
-
-
-class Token(models.Model):
-    # token_id = models.BigAutoField()
-    token = models.CharField(max_length=200)
-    waiting_id = models.ForeignKey(Waiting, primary_key=True, on_delete=models.CASCADE, db_column='token_id')
-
-    class Meta:
-        db_table = 'token'
 
 
 class UserManager(BaseUserManager):
@@ -27,7 +17,9 @@ class UserManager(BaseUserManager):
             raise ValueError('The Email must be set')
 
         user = self.model(
-            email=self.normalize_email(email)
+            email=self.normalize_email(email),
+            name=name,
+            phone_num=phone_num
         )
         user.set_password(password)
         # users.set_is_staff(False)
@@ -50,9 +42,14 @@ class UserManager(BaseUserManager):
 
 
 class User(AbstractBaseUser):
+    class Role(models.TextChoices):
+        AUTH = 'auth'
+        STORE = 'store'
+
     name = models.CharField(max_length=50)
     phone_num = models.CharField(max_length=13)
     email = models.EmailField(unique=True, max_length=255)
+    role = models.CharField(max_length=10, default='auth', choices=Role.choices)
     is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
