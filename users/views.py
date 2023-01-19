a3de34e1ed604721da57953b9c2af3e882176cf2
ggamD00 <jaehuek@naver.com>
2023-01-20 03:40:41 +0900

    feat: add JWT

--- users/views.py
+++ users/views.py
@@ -1,4 +1,3 @@
-# 회원가입
 from django.contrib.auth import authenticate
 from rest_framework import status
 from rest_framework.permissions import AllowAny
@@ -6,7 +5,22 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
-from users.serializer import SignupSerializer, SigninSerializer
+from users.models import User
+from users.serializer import SignupSerializer
+
+
+# jwt 토큰 생성
+def make_token(user):
+    token = TokenObtainPairSerializer.get_token(user)
+    refresh_token = str(token)
+    access_token = str(token.access_token)
+    res = Response(
+        {
+            "access": access_token,
+            "refresh": refresh_token,
+        }
+    )
+    return res
 
 
 class SignupAPIView(APIView):
@@ -16,44 +30,25 @@ class SignupAPIView(APIView):
         serializer = SignupSerializer(data=request.data)
         if serializer.is_valid():
             user = serializer.save()
-            # jwt token 접근해주기
-            token = TokenObtainPairSerializer.get_token(user)
-            refresh_token = str(token)
-            access_token = str(token.access_token)
-            res = Response(
-                {
-                    "access": access_token,
-                    "refresh": refresh_token,
-                }
-            )
-            res.set_cookie("refresh", refresh_token, httponly=True)
-            return Response(res.data, status=status.HTTP_201_CREATED)
+            token = make_token(user)
+            return Response(token.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-# 로그인
 class SigninView(APIView):
+    permission_classes = [AllowAny]
 
     def post(self, request):
         user = authenticate(
             email=request.data.get("email"), password=request.data.get("password")
         )
         if user is not None:
-            serializer = SigninSerializer(user)
-            token = TokenObtainPairSerializer.get_token(user)
-            refresh_token = str(token)
-            access_token = str(token.access_token)
-            res = Response(
-                {
-                    "user": serializer.data,
-                    "message": "login success",
-                    "token": {
-                        "access": access_token,
-                        "refresh": refresh_token,
-                    },
-                },
-                status=status.HTTP_200_OK,
-            )
-            return res
+            user_data = User.objects.get(email=request.data.get("email"))
+            role = user_data.role
+            if role == 'auth':
+                token = make_token(user)
+                return Response(token.data, status=status.HTTP_200_OK)
+            else:
+                return Response(status=status.HTTP_401_UNAUTHORIZED)
         else:
             return Response(status=status.HTTP_400_BAD_REQUEST)
