4e99e8bb4b63f9698e1e942016f5059cb99be6a2
ggamD00 <jaehuek@naver.com>
2023-01-26 03:08:53 +0900

    chore: Change response body in signup and signin of user

--- users/serializer.py
+++ users/serializer.py
@@ -4,6 +4,12 @@ from rest_framework import serializers
 User = get_user_model()
 
 
+class SigninSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = '__all__'
+
+
 class SignupSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
@@ -26,7 +32,7 @@ class SignupSerializer(serializers.ModelSerializer):
         return user
 
 
-class StoreSignupSerializer(serializers.ModelSerializer):
+class StoreUserSignupSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = '__all__'
--- users/views.py
+++ users/views.py
@@ -9,21 +9,7 @@ from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
 from swagger.serializer import SwaggerUserSignupSerializer, SwaggerUserSigninSerializer
 from users.models import User
-from users.serializer import SignupSerializer
-
-
-# jwt 토큰 생성
-def make_token(user):
-    token = TokenObtainPairSerializer.get_token(user)
-    refresh_token = str(token)
-    access_token = str(token.access_token)
-    res = Response(
-        {
-            "access": access_token,
-            "refresh": refresh_token,
-        }
-    )
-    return res
+from users.serializer import SignupSerializer, SigninSerializer
 
 
 class Signup(APIView):
@@ -35,8 +21,20 @@ class Signup(APIView):
         serializer = SignupSerializer(data=request.data)
         if serializer.is_valid():
             user = serializer.save()
-            token = make_token(user)
-            return Response(token.data, status=status.HTTP_201_CREATED)
+            token = TokenObtainPairSerializer.get_token(user)
+            refresh_token = str(token)
+            access_token = str(token.access_token)
+            res = Response(
+                {
+                    "user": serializer.data,
+                    "message": "register successs",
+                    "access": access_token,
+                },
+                status=status.HTTP_201_CREATED,
+            )
+            # jwt 토큰 => 쿠키에 저장
+            res.set_cookie("refresh", refresh_token, httponly=True)
+            return res
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
@@ -51,10 +49,23 @@ class Signin(APIView):
         )
         if user is not None:
             user_data = User.objects.get(email=request.data.get("email"))
+            serializer = SigninSerializer(user)
             role = user_data.role
             if role == 'auth':
-                token = make_token(user)
-                return Response(token.data, status=status.HTTP_200_OK)
+                token = TokenObtainPairSerializer.get_token(user)
+                refresh_token = str(token)
+                access_token = str(token.access_token)
+                res = Response(
+                    {
+                        "user": serializer.data,
+                        "message": "login success",
+                        "access": access_token,
+                    },
+                    status=status.HTTP_200_OK,
+                )
+                # jwt 토큰 => 쿠키에 저장
+                res.set_cookie("refresh", refresh_token, httponly=True)
+                return res
             else:
                 return Response(status=status.HTTP_401_UNAUTHORIZED)
         else:
