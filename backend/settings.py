1fa342307b2a956c165ee9ae47729ffc3895ea4c
chynmn <chynmn0523@gmail.com>
2023-02-02 04:14:31 +0900

    [hotfix] add server ip address for cors_error

--- backend/settings.py
+++ backend/settings.py
@@ -206,7 +206,10 @@ CORS_ORIGIN_WHITELIST = [
     'http://localhost:8000',
     'http://127.0.0.1:80',
     'http://localhost:80',
+    'http://15.164.28.246:3000',
+    'http://15.164.28.246',
 ]
+CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True
 
 AUTH_USER_MODEL = 'users.User'
